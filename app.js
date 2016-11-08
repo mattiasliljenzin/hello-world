36bc3b3a3ed756fa515cda003c3855fbfc9fb214
jamesblund <mattias.liljenzin@gmail.com>
2016-11-08 20:35:35 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+const port = process.env.port || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+})
