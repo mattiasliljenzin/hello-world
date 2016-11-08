604c079e08969995944fb31c1419583dea191c5e
jamesblund <mattias.liljenzin@gmail.com>
2016-11-08 21:31:30 +0100

    fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+var port = process.env.port || 1337;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(process.env.port || 1337, function () {
-  console.log('Example app listening on port ' + process.env.port || 1337);
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 })
