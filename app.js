d0fd7f2a3ef1e6bf3f7b1cb4c5c29611f943c617
jamesblund <mattias.liljenzin@gmail.com>
2016-11-08 21:24:44 +0100

    updated port

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-const port = process.env.port || 3000;
-
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+app.listen(process.env.port || 1337, function () {
+  console.log('Example app listening on port ' + process.env.port || 1337);
 })
